5431bf227ee84860c6ae9260afb5e55d47e8efef
Pagesixgroup <pagesixgroup@yahoo.com>
2026-04-30 11:52:57 -0500

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -2,8 +2,6 @@ const CACHE_NAME = 'avcommand-v1';
 const STATIC_ASSETS = [
   '/',
   '/manifest.json',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png',
 ];
 
 self.addEventListener('install', (event) => {
